0c4490825796c6dfb38d11607ff176e3ae2ad4f0
Gription <gription@users.noreply.github.com>
2025-01-14 13:50:18 -0500

    Small cleanups but linting problem resolved.  Back to ~30 'issues' out of <200 LOC.  .  .  .  Breathtaking..

--- main.go
+++ main.go
@@ -7,28 +7,23 @@ import (
 func main() {
 	fmt.Printf("\n -=& Begin: carddeck v0.2.. *=-\n")
 	theDeck := createDeck()
-
-	//, Create x players, each holding x cards.  highRollers[p#][hand][cards]{card-details}
-	highRollers := []player{} //?  {} at end?  struct?
+	players := []player{} //                    //?  {} at end?  struct?
 	for i := 0; i < numberOfPlayers; i++ {
 		p := player{idx: i}
-		highRollers = append(highRollers, p)
-		theDeck, highRollers[i].hand = deal(theDeck, cardsPerPlayer)
+		players = append(players, p)
+		theDeck, players[i].hand = deal(theDeck, cardsPerPlayer)
 		fmt.Println()
-
-		// fmt.Println("highRollers #: ", highRollers[i].idx, "hand: ", highRollers[i].hand)
+		// fmt.Println("players #: ", players[i].idx, "hand: ", players[i].hand)
 	}
 
 	// fmt.Print("\n\n")
-	// fmt.Println("highRollers #: ", highRollers[1].idx, "- hand: ", highRollers[1].hand)
-	// fmt.Println("highRollers #: ", highRollers[2].idx, "- hand: ", highRollers[2].hand)
-	// fmt.Println("highRollers #: ", highRollers[3].idx, "- hand: ", highRollers[3].hand)
-	// fmt.Println("highRollers #: ", highRollers[0].idx, "- hand: ", highRollers[0].hand)
+	// fmt.Println("players #: ", players[1].idx, "- hand: ", players[1].hand)
+	// fmt.Println("players #: ", players[2].idx, "- hand: ", players[2].hand)
+	// fmt.Println("players #: ", players[3].idx, "- hand: ", players[3].hand)
+	// fmt.Println("players #: ", players[0].idx, "- hand: ", players[0].hand)
 
 	fmt.Print("\nOne lucky card:")
-	logCard(highRollers[0].hand[1], true)
+	logCard(players[0].hand[1], true)
 
 	logDeck(theDeck, "full", true)
 }
-
-// ? are all linters engaging?
